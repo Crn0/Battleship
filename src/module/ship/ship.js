926406c85966c236cd7061dfa0412f320a1319d3
EssenceCode <123456789+odin@users.noreply.github.com>
2023-12-25 15:05:35 +0800

    Create a hit function to increase the hits the ship took

--- src/module/ship/ship.js
+++ src/module/ship/ship.js
@@ -1,8 +1,15 @@
 export default function CreateShip(name, length) {
-    
+    let health = 0;
+
+    const hit = () => {
+        health += 1;
+        
+        return true;
+    };
 
     return Object.freeze({
         get name() { return name },
         get length() { return length },
+        hit,
     });
 };
