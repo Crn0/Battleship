4b80c2e2ab236f1e49dd4a99ebdca89fba4d0914
EssenceCode <123456789+odin@users.noreply.github.com>
2024-01-02 18:30:49 +0800

    Create a property to attack enemy board

--- src/module/player/player.js
+++ src/module/player/player.js
@@ -4,11 +4,33 @@ export default function Player(name, board1, board2, isComputer = false) {
     const board = board1;
  
     const enemyBoard = board2;
+
+    const atkEnemy = (row, col) => {
+        if(isAi) {
+            let randomRow = Math.floor(Math.random() * 10);
+            let randomCol = Math.floor(Math.random() * 3);
+       
+            while(enemyBoard.shipAttacks[randomRow][randomCol] === true) {
+                randomRow = Math.floor(Math.random() * 10);
+                randomCol = Math.floor(Math.random() * 10);
+            }
+           
+            return enemyBoard.receivedAtk(randomRow, randomCol);
+        };
+
+        if(enemyBoard.shipAttacks[row][col] === false) {
+            
+            return enemyBoard.receivedAtk(row, col);
+        };
+        
+        return false;
+    };
  
      return Object.freeze({
          get name() { return name},
          get isAi() { return isAi},
          get board() { return board},
          get enemyBoard() { return enemyBoard},
+         atkEnemy
      });
  };
