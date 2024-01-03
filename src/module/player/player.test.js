ae77ed83a1c4076fbd77e3282cee1840bbfbb9e7
EssenceCode <123456789+odin@users.noreply.github.com>
2024-01-03 16:33:17 +0800

    Refactor computer attack test

--- src/module/player/player.test.js
+++ src/module/player/player.test.js
@@ -53,19 +53,28 @@ describe("Create player", () => {
 
 describe("Players attack", () => {
     test("human player attacks computer player", () => {
-        expect(player1.atkEnemy(0, 0)).toBe(true);
+        player1.atkEnemy(0, 0)
+        expect(player2.board.shipAttacks[0][0]).toBe(true);
     });
 
     test("human player cannot attack the same coord twice", () => {
-        expect(player1.atkEnemy(0, 0)).toBe(true);
+       
         expect(player1.atkEnemy(0, 0)).toBe(false);
     });
 
     test("computer player attacks human player", () => {
-        expect(player2.atkEnemy()).toBe(true);
-        expect(player2.atkEnemy()).toBe(true);
-        expect(player2.atkEnemy()).toBe(true);
-        console.log(board1.dock.length)
+        const mockAtk = jest.fn(() => player2.atkEnemy());
+        mockAtk();
+        mockAtk();
+        mockAtk();
+        // store the attacks;
+        const attacksArr = [];
+        // map the ship attacks then push to the attack array
+        const attack = player1.board.shipAttacks.map((row) => row.map((col) => attacksArr.push(col)));
+        // check if every cell is false
+        // will return false, if the computer land a hit in human board
+        const checkAtk = attacksArr.every(cell => cell === false);
+        expect(checkAtk).toBe(false);
     });
 
 });
