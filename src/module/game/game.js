33c5208425fb4a41d2d9747cb559520f7ccda41e
EssenceCode <123456789+odin@users.noreply.github.com>
2024-01-10 12:22:21 +0800

    Finish game function

--- src/module/game/game.js
+++ src/module/game/game.js
@@ -1,30 +1,38 @@
 /* eslint-disable no-unused-vars */
 import CreateBoard from "../board/board";
 import CreatePlayer from "../player/player";
+import Helper from "../helper/helper";
 
 export default function Game() {
     let isGameStarted = false;
+    let currentPlayer;
+
     const players = [];
     const boards = [];
-    let currentPlayer
+
+    const wait = (ms) => Helper().wait(ms);
+
     // initialized boards and players
     if(isGameStarted === false) {
         boards.push(CreateBoard());
         boards.push(CreateBoard());
+       
 
         players.push(CreatePlayer("human", boards[0], boards[1]));
-        players.push(CreatePlayer("computer",boards[1],boards[0], false));
-
+        players.push(CreatePlayer("computer",boards[1],boards[0], true));
+        
         players[0].createAllShips();
         players[1].createAllShips();
 
-
-        currentPlayer = {...players[0]}
-
+        currentPlayer = {...players[0]} 
         isGameStarted = true;
+        
     };
 
+    
     const getActivePlayer = () => currentPlayer;
+
+    const isSunkAll = (player = getActivePlayer()) => player.board.shipWrecks();
     
     const switchPlayer = () => {
         if(currentPlayer.name === players[0].name) {
@@ -36,8 +44,29 @@ export default function Game() {
         return currentPlayer;
     };
 
+    const humanInput = async (row, col) => {
+        await wait(1)
+        players[0].atkEnemy(row, col);
+        switchPlayer()
+        return true;
+    };
+
+    const computerInput = async () => {
+        if(isSunkAll()) return false;
+        await wait(1);
+        players[1].atkEnemy();
+        switchPlayer()
+        return true
+    };
+
+    
     return Object.freeze({
-        switchPlayer,
+        get players() { return players},
         getActivePlayer,
+        switchPlayer,
+        isSunkAll,
+        humanInput,
+        computerInput,
+        
     });
 };
