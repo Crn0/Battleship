4db44309dc8ff07465dbe9415c0b575eb06d6ee6
crno <118383162+Crn0@users.noreply.github.com.>
2025-08-10 16:43:51 +0800

    commit

--- dist/module/dom/controller.d.ts
+++ dist/module/dom/controller.d.ts
@@ -2,7 +2,6 @@ import type { IPlayer } from "../player/player";
 import { type IGame } from "../game/game";
 export declare const domController: () => {
     gameController: IGame;
-    domState: void;
-    shipPlacement(player: IPlayer, enemyId: string, random?: boolean): void;
+    shipPlacement(player: IPlayer, random?: boolean): void;
 };
 //# sourceMappingURL=controller.d.ts.map
