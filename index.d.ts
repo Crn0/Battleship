8470816944fc5c0390040ff4aab72c3962b7213c
crno <118383162+Crn0@users.noreply.github.com.>
2025-08-10 16:43:51 +0800

    commit

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
+import "./module/dom/game-container";
 import "./style.css";
 //# sourceMappingURL=index.d.ts.map
--- module/dom/ship-visibility.d.ts
+++ module/dom/ship-visibility.d.ts
@@ -1,4 +1,4 @@
 import type { IPlayer } from "../player/player.ts";
-export declare const showShips: (player: IPlayer) => (Element | undefined)[];
+export declare const showShips: (player: IPlayer) => void;
 export declare const hideShips: (player: IPlayer) => (Element | undefined)[];
 //# sourceMappingURL=ship-visibility.d.ts.map
